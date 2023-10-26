e936b0f079a66c3c5db313e08a5e2247656caf3e
Regis Andrade <regis.batista.andrade@outlook.com>
2023-10-25 21:17:18 -0300

    Definição  dos Designer Grid  Principal,  Criação  do Card de Tempo

---
+++ Componentes/Card_Clima.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClimaQuente.Componentes
+{
+    /// <summary>
+    /// Interação lógica para Card_Clima.xam
+    /// </summary>
+    public partial class Card_Clima : UserControl
+    {
+        public Card_Clima()
+        {
+            InitializeComponent();
+        }
+    }
+}
